7a074e47a9d8c74ecb31c4d9b537118e1973ddb0
Chaitali <chaitalipatel18j@gmail.com>
2017-01-21 14:08:12 +1300

    Functionality added on buuton click

---
+++ app.js
@@ -0,0 +1,46 @@
+(function () {
+	'use strict';
+
+	angular.module('LunchCheck', [])
+	.controller('LunchCheckController', LunchCheckController);
+
+	LunchCheckController.$inject = ['$scope'];
+
+	function LunchCheckController($scope) {
+		
+		$scope.msg = "Please enter data first";
+
+		$scope.checkIfTooMuch = function () {
+
+			if($scope.list == "" || $scope.list == undefined){
+				$scope.msg = "Please enter data first!";
+			}
+
+			else {
+
+				var dishes = $scope.list.split(",");
+
+				if (dishes.length <=3) {
+					$scope.msg = "Enjoy!";			
+				}
+				else {
+					$scope.msg = "Too much!";
+				}				
+			}	
+			setColor();
+		}
+		
+		var setColor = function(){
+			$scope.customStyle ={};
+			if($scope.msg == "Enjoy!" || $scope.msg == "Too much!" ){
+				$scope.customStyle.style = {"color":"green"};
+				$scope.customStyle.borderStyle = {"border": "1px solid green"};
+			}
+			else {
+				$scope.customStyle.style = {"color":"red"};
+				$scope.customStyle.borderStyle  = {"border": "1px solid red"};
+			}
+		}
+		setColor();			
+	};
+})();
